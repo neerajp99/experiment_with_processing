578f273d9ba06813e6cdb29b773c6b0a43cb8adb
neerajp99 <neerajp1999@gmail.com>
2019-09-14 22:37:08 +0530

    Add sketch

--- examples/particles/sketch.js
+++ examples/particles/sketch.js
@@ -3,10 +3,12 @@ let particle;
 function setup () {
 	createCanvas(600, 400);
 	particle = new Particle();
-}
+} 
 
 function draw () {
 	background(56);
+	let gravity = createVector(0.2, .5)
+	partile.applyForce(gravity);
 	particle.update();
 	particle.display();
 }
