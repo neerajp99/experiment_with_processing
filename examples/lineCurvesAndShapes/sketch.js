f11e85955017bfe2b4fc08b23d079aa6ec72aacf
neerajp99 <neerajp1999@gmail.com>
2019-12-26 01:16:24 +0530

    Modify sketch

--- examples/lineCurvesAndShapes/sketch.js
+++ examples/lineCurvesAndShapes/sketch.js
@@ -4,17 +4,24 @@ function setup() {
 }
 
 function draw() {
+  // Loop over the Y-coordinates
   for (let i = 1.5; i < 500; i += 2) {
     noFill();
+    // Begin shape for curve vertex
     beginShape();
     strokeWeight(0.1);
+    // Initial x, y coordinates
     let xLabel = 0;
     let yLabel = 0;
+    // Loop over the Y-coordinates
     for (let j = 5; j < 680; j += 5) {
+      // Add random colors
       randomColor = color(random(255), random(255), random(255));
       stroke(randomColor);
+      // increment the values of x,y coordinates
       xLabel = 30 + j;
       yLabel = i * 20 + random(-2, 2);
+      // Create curved lines
       if (xLabel < 470 && yLabel < 680) {
         noFill();
         curveVertex(xLabel, yLabel);
@@ -22,10 +29,11 @@ function draw() {
       }
       endShape();
       colorMode(RGB, 360, 360, 200);
+      // Create random ellipses
       if (random(1) < 0.2 && xLabel < 470 && yLabel < 680) {
         randomColor = color(random(255), random(255), random(255));
         fill(randomColor);
-        circle(xLabel, yLabel, 13);
+        ellipse(xLabel, yLabel, 13, 13);
         noFill();
       }
 
